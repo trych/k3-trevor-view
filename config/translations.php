3330d4539362f8fd712b4fa0d0c85cdec198c35a
trych <timorychert@23x5.net>
2022-11-26 00:04:18 +0100

    Translation array bugfix

--- config/translations.php
+++ config/translations.php
@@ -4,5 +4,5 @@ return [
     'en' => require __DIR__ . '/translations/en.php',
     'fr' => require __DIR__ . '/translations/fr.php',
     'it' => require __DIR__ . '/translations/it.php',
-    'it' => require __DIR__ . '/translations/de.php'
+    'de' => require __DIR__ . '/translations/de.php'
 ];
